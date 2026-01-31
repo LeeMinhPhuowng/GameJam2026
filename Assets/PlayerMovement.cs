75c1f6f36b1f1b6fd6da1f877677b05bf8af4013
conmeo <lmp1972005@gmail.com>
2026-01-31 15:13:03 +0700

    3h12
    Game UI

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -33,8 +33,8 @@ public class PlayerMovement : MonoBehaviour
         if (!isAlive) return;
         Run();
         FlipSprite();
-        ClimbLadder();
-        Die();
+        //ClimbLadder();
+        //Die();
     }
 
     void OnMove(InputValue value)
